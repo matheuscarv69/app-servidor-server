72e995435c907f771e34f68098f6158fa98a1e24
matheuscarv69 <matheus9126@gmail.com>
2020-11-13 13:09:53 -0400

    Configuration Maven Profiles

--- src/main/java/io/github/matheuscarv69/AppServidorApplication.java
+++ src/main/java/io/github/matheuscarv69/AppServidorApplication.java
@@ -2,11 +2,13 @@ package io.github.matheuscarv69;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.SpringServletContainerInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class AppServidorApplication {
+public class AppServidorApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
